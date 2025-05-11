7ea5ac5f5905a793f1400ac5d4c626829d99b5de
Artem <70117318+044RoHS@users.noreply.github.com>
2025-05-11 12:59:04 +0300

    Second  commit

--- header.h
+++ header.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <vector>
-#include<iostream>
+#include <iostream>
 
 template <typename T>
 class DvumerniMassiv {
@@ -15,47 +15,16 @@ public:
         : rows(a), cols(b), data(a, std::vector<T>(b)) {}
 
 
-    class RowProxy {
-    private:
-        std::vector<T>& row;
-
-    public:
-        explicit RowProxy(std::vector<T>& r) : row(r) {}
-
-        T& operator[](size_t col) {
-            return row[col];
-        }
-    };
-
-
-    class ConstRowProxy {
-    private:
-        const std::vector<T>& row;
-
-    public:
-        explicit ConstRowProxy(const std::vector<T>& r) : row(r) {}
-
-        const T& operator[](size_t col) const {
-            return row[col];
-        }
-    };
-
-
-    RowProxy operator[](size_t row) {
-        return RowProxy(data[row]);
+    T* operator[](size_t row) {
+        return data[row].data();
     }
 
 
-    ConstRowProxy operator[](size_t row) const {
-        return ConstRowProxy(data[row]);
+    const T* operator[](size_t row) const {
+        return data[row].data();
     }
 
 
-    size_t Rows() const {
-        return rows;
-    }
-
-    size_t Cols() const {
-        return cols;
-    }
+    size_t Rows() const { return rows; }
+    size_t Cols() const { return cols; }
 };
